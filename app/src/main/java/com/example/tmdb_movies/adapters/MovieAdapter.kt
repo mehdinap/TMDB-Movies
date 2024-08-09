b4dc7b79a73ba8b295218e66bb87b251c894e404
mehdinajibpour <najibpour6@gmail.com>
2024-08-09 23:18:41 +0330

    makes adapter for transform responce

---
+++ app/src/main/java/com/example/tmdb_movies/adapters/MovieAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.tmdb_movies.adapters
+
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import com.example.tmdb_movies.model.Movie
+import com.example.tmdb_movies.model.MovieResponse
+import kotlinx.serialization.json.Json
+import retrofit2.Call
+import retrofit2.Response
+
+class MovieAdapter {
+    companion object {
+        @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+        fun moviesOfResponse(response: Response<MovieResponse>): List<Movie> {
+            if (response.isSuccessful) {
+                val movieResponse = response.body()
+                return movieResponse?.results ?: emptyList()
+            } else {
+                throw Exception("Failed to load movies: ${response.errorBody()?.string()}")
+            }
+
+        }
+    }
+}
