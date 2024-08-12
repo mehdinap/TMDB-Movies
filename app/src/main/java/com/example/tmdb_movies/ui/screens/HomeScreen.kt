c03accdd18baf3ca2e821f7a0fab0316eb4c95a7
mehdinajibpour <najibpour6@gmail.com>
2024-08-13 00:43:05 +0330

    Fixes some import conflicts

--- app/src/main/java/com/example/tmdb_movies/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/tmdb_movies/ui/screens/HomeScreen.kt
@@ -1,20 +1,21 @@
 package com.example.tmdb_movies.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
