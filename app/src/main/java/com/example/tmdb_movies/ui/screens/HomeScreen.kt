b149123081f620de88dd39b36fdadeecf92ecce9
mehdinajibpour <najibpour6@gmail.com>
2024-08-15 19:03:16 +0330

    Makes DetailScreen and DetailViewModel to shows movie details.

--- app/src/main/java/com/example/tmdb_movies/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/tmdb_movies/ui/screens/HomeScreen.kt
@@ -1,48 +1,33 @@
 package com.example.tmdb_movies.ui.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import com.example.tmdb_movies.R
 import com.example.tmdb_movies.data.AppType
 import com.example.tmdb_movies.model.Genre
-import com.example.tmdb_movies.model.Movie
 import com.example.tmdb_movies.ui.MovieUiState
-import com.example.tmdb_movies.ui.theme.TMDBMoviesTheme
-
+val navigationItemContentList = listOf(
+    NavigationItemContent(appType = AppType.Movie, text = "Movie"),
+    NavigationItemContent(appType = AppType.TV, text = "TV"),
+)
 @Composable
 fun HomeScreen(
     movieCategories: List<MovieCategory>,
@@ -53,10 +38,7 @@ fun HomeScreen(
     cardClicked: () -> Unit,
     contentPadding: PaddingValues = PaddingValues(0.dp),
 ) {
-    val navigationItemContentList = listOf(
-        NavigationItemContent(appType = AppType.Movie, text = "Movie"),
-        NavigationItemContent(appType = AppType.TV, text = "TV"),
-    )
+
     if (movieCategories.any { it.uiState is MovieUiState.Error }) {
         ErrorScreen(
             retryAction = retryAction, modifier = Modifier.fillMaxSize()
@@ -100,7 +82,7 @@ fun HomeScreen(
             Row(Modifier.weight(6.5F)) {
                 BottomNavigationBar(
                     currentTab = AppType.Movie,
-                    onTabPressed = { appType ->
+                    onTabPressed = { newTab ->
                     },
                     navigationItemContentList = navigationItemContentList,
                     modifier = Modifier
@@ -114,41 +96,60 @@ fun HomeScreen(
 
 
 @Composable
-private fun BottomNavigationBar(
+fun BottomNavigationBar(
     currentTab: AppType,
     onTabPressed: (AppType) -> Unit,
     navigationItemContentList: List<NavigationItemContent>,
     modifier: Modifier = Modifier
 ) {
+    val gradientColors = listOf(Cyan, Color.Blue, Color.Red)
+
+
     NavigationBar(modifier = modifier) {
         for (navItem in navigationItemContentList) {
             NavigationBarItem(
                 selected = currentTab == navItem.appType,
                 onClick = { onTabPressed(navItem.appType) },
-                icon = { Text(navItem.text) },
+                icon = {
+                    Text(
+                        text = navItem.text,
+                        style = if (currentTab == navItem.appType) {
+                            TextStyle(
+                                brush = Brush.linearGradient(
+                                    colors = gradientColors
+                                )
+                            )
+                        } else {
+                            MaterialTheme.typography.bodySmall.copy(color = Color.Gray)
+                        }
+                    )
+                },
             )
         }
     }
+
 }
 
 data class NavigationItemContent(
     val appType: AppType, val text: String
 )
 
-//@Preview(showBackground = true)
+//@Preview
 @Composable
-fun LoadingScreenPreview() {
-    TMDBMoviesTheme {
-        LoadingScreen()
-    }
-}
-
-//@Preview(showBackground = true)
-@Composable
-fun ErrorScreenPreview() {
-    TMDBMoviesTheme {
-        ErrorScreen({})
-    }
+fun previewNavigation() {
+    val navigationItemContentList = listOf(
+        NavigationItemContent(appType = AppType.Movie, text = "Movie"),
+        NavigationItemContent(appType = AppType.TV, text = "TV"),
+    )
+    BottomNavigationBar(
+        AppType.Movie,
+        { appType ->
+        },
+        navigationItemContentList = navigationItemContentList,
+        modifier = Modifier
+            .height(50.dp)
+            .fillMaxWidth()
+    )
 }
 
 //@Preview(showBackground = true)
